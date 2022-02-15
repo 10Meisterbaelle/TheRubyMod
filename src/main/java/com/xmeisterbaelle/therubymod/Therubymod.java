8d4491768c9b773674457231ec60f1c5cad1636b
10Meisterbaelle <neumaif02@gmail.com>
2022-02-15 10:06:37 +0100

    Added Ruby Ore; no texture

--- src/main/java/com/xmeisterbaelle/therubymod/Therubymod.java
+++ src/main/java/com/xmeisterbaelle/therubymod/Therubymod.java
@@ -4,18 +4,21 @@ import com.xmeisterbaelle.therubymod.items.RubySwordItem;
 import com.xmeisterbaelle.therubymod.tools.RubySwordTool;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.SwordItem;
-import net.minecraft.item.ToolItem;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class Therubymod implements ModInitializer {
     public static ToolItem RUBY_SWORD = new SwordItem(RubySwordTool.INSTANCE, 5, 96F, new Item.Settings().group(ItemGroup.COMBAT));
+    public static final Block RUBY_ORE = new Block(FabricBlockSettings.of(Material.METAL).strength(3.5F, 3));
 
     @Override
     public void onInitialize() {
         Registry.register(Registry.ITEM, new Identifier("therubymod", "ruby_sword"), RUBY_SWORD);
+        Registry.register(Registry.BLOCK, new Identifier("therubymod", "ruby_ore"), RUBY_ORE);
+        Registry.register(Registry.ITEM, new Identifier("therubymod", "ruby_ore"), new BlockItem(RUBY_ORE, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
     }
 }
